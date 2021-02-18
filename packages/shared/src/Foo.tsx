52a0fdba7e88d2ff52e2f2c866f148051b09be03
김태연 <ty980407@gmail.com>
2021-02-18 19:50:18 +0900

    ✨ Foo 컴포넌트 생성

---
+++ packages/shared/src/Foo.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+const Foo = () => (
+  <div
+    style={{
+      color: 'white',
+      fontSize: 40,
+      width: '100%',
+      position: 'fixed',
+      top: 100,
+      textAlign: 'center',
+    }}
+  >
+    Foo! Foo! Foo! Foo! Foo! Foo!
+  </div>
+);
+export default Foo;
